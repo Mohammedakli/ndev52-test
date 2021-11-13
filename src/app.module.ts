fd9186918dc852740c83361633814e5275d71508
Momo <mohammedakli@gmail.com>
2021-11-13 12:13:37 +0100

    Ajout du fichier .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
-import { UserService } from './services/user.service';
-import { UserController } from './controllers/user.controller';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./schemas/user.schema";
+import { UserService } from "./services/user.service";
+import { UserController } from "./controllers/user.controller";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:HK3kwD0WBv3VgV4q@cluster.jtuwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_LINK),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
