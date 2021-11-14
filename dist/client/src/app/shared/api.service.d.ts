fb0dea51e46774419e47b929d5e7eab31ddafe67
Momo <mohammedakli@gmail.com>
2021-11-14 17:41:13 +0100

    deplacer le dossier dist du client

--- dist/client/src/app/shared/api.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-export declare class ApiService {
-    private httpClient;
-    constructor(httpClient: HttpClient);
-    postUser(data: any): import("rxjs").Observable<any>;
-    getUser(): import("rxjs").Observable<any>;
-    updateUser(data: any, _id: number): import("rxjs").Observable<any>;
-    deleteUser(_id: number): import("rxjs").Observable<any>;
-}
--- dist/client/src/environments/environment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: false,
-    backendUrl: 'http://localhost:3000/api',
-};
-//# sourceMappingURL=environment.js.map
