55d641af56f764fc550135b0d6d17128c501e873
Momo <mohammedakli@gmail.com>
2021-11-15 16:35:16 +0100

    modifier le fichier .env

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-  await app.listen(process.env.APP_PORT);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
