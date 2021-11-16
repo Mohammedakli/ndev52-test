0c012ae6ee47005ceb98395c21a4ec89af122dc8
Momo <mohammedakli@gmail.com>
2021-11-16 11:20:20 +0100

    modification fichier app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ dotenv.config();
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "../../dist/", "client"),
+      rootPath: join(__dirname, "../../../dist/", "client"),
     }),
     MongooseModule.forRoot(process.env.MONGODB_LINK),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
